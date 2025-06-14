7abb23542f56d400144d81d6634887db07fa14eb
lumalin022 <lumalin022@student.wethinkcode.co.za>
2025-06-14 12:24:58 +0200

    init push

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200)
+       .send('Welcome to my Nelson API by Lunga Malinga');
+});
+
+app.get('/data', (req, res) => {
+    res.status(200)
+       .send('This is a gest request. Please use a POST request to send data.');
+})
+
+app.post('/data', (req, res) => {
+    const data = req.body;
+    
+    //handle missing body
+    if (!data || !data.data){
+        return res.status(400)
+                  .send('Bad request: Missing "data" field.')
+    }
+
+    // !return sorted array
+    const response_array = generate_response(data.data)
+    
+    return res.status(200)
+        .send(response_array);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT : ${PORT}`);
+})
+
+// ! response handler
+function generate_response(data){
+    return {
+        word: data.split('').sort()
+    };
+}
